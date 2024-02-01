f4cf034c85ae115ec038d10fa37574205cd50651
LolkaKot <annaredhead888@gmail.com>
2024-02-02 00:26:37 +0300

    defw

--- задание3/obj/Debug/net8.0/задание3.AssemblyInfo.cs
+++ задание3/obj/Debug/net8.0/задание3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("задание3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+311eabe292019743ecfa9a7a33dde85186cb2ec2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d0cb7633445969f28035b20ccae9de1253ebc79")]
 [assembly: System.Reflection.AssemblyProductAttribute("задание3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("задание3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
