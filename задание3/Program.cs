311eabe292019743ecfa9a7a33dde85186cb2ec2
LolkaKot <annaredhead888@gmail.com>
2024-02-01 22:58:05 +0300

    dv

--- задание3/Program.cs
+++ задание3/Program.cs
@@ -1,1 +1,43 @@
-﻿
+﻿// ====================================
+Console.WriteLine("Задание №1");
+
+double L1, e1, x1, a1, g1, c1;
+
+Console.WriteLine("Введите значение перременной e");
+e1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение перременной x");
+x1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение перременной a");
+a1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение перременной g");
+g1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение перременной c");
+c1 = Convert.ToDouble(Console.ReadLine());
+
+L1 = (Math.Sqrt(Math.Pow(e1,x1)-Math.Pow((Math.Cos(Math.Pow(x1,2)*Math.Pow(a1,5))),4)))+Math.Pow((Math.Atanh(a1-Math.Pow(x1,5))),4)/(Math.Pow((Math.Abs(a1+x1*Math.Pow(c1,4))), e1/2));
+
+Console.WriteLine($"Результат задания №1 = {L1}");
+
+
+// ====================================
+Console.WriteLine("Задание №2");
+
+double L2, c2, x2, t2;
+
+Console.WriteLine("Введите значение перременной e");
+e1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение перременной x");
+x1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение перременной a");
+a1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение перременной g");
+g1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение перременной c");
+c1 = Convert.ToDouble(Console.ReadLine());
+
+L2 = ()/();
+
+Console.WriteLine($"Результат задания №2 = {L2}");
+
+
+// ====================================
--- задание3/obj/Debug/net8.0/задание3.AssemblyInfo.cs
+++ задание3/obj/Debug/net8.0/задание3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("задание3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ff9fa63d95e9dfd6271917c937879947ba60700")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+413904c6b505c21e0c129c2ea6ec234a29b543a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("задание3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("задание3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
